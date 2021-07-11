3aa90dd7aa2843e01933636d50701443c372675b
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-07-11 14:29:12 -0400

    did the darkmode button styles
    also added the background pattern

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
-document.getElementById("clickMe").addEventListener("click", () => {
+document.getElementById("mode-btn").addEventListener("click", () => {
     document.querySelector("body").classList.toggle("lightTheme");
+    document.getElementById("mode-btn").classList.toggle("active");
 });
